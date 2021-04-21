f11c4a5511d57cadcdec55ef846e40aac2026c5d
Viktoria-Iudina <jajudinaja@gmail.com>
2021-04-21 14:54:08 +0200

    Added Signature component

---
+++ src/Signature.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+export default function Signature() {
+  return (
+    <div className="signature">
+      <small>
+        <a href="https://github.com/Viktoria-Iudina/project" target="_blank">
+          Open-source code
+        </a>
+        , by Viktoriia Iudina
+      </small>
+    </div>
+  );
+}
