ddd8c27d45f6a4cb83d86cd0b43900245dbc0a93
Viktoria-Iudina <jajudinaja@gmail.com>
2021-04-21 15:15:01 +0200

    Added rel='noreferrer noopener' attribute to target='_blank' in Signature component

--- src/Signature.js
+++ src/Signature.js
@@ -5,7 +5,7 @@ export default function Signature() {
   return (
     <div className="signature">
       <small>
-        <a href="https://github.com/Viktoria-Iudina/project" target="_blank">
+        <a href="https://github.com/Viktoria-Iudina/project" target='_blank' rel="noopener noreferrer">
           Open-source code
         </a>
         , by Viktoriia Iudina
